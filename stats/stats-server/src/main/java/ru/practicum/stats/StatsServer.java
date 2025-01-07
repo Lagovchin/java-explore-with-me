cb58b3bd24230ceab7b4af09de2087ff27f2cdf0
Lagovchin <lagovchin95@yandex.ru>
2025-01-07 14:39:58 +0300

    stats6

--- stats/stats-server/src/main/java/ru/practicum/stats/StatsServer.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/StatsServer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "ru.practicum.stats-dto")
+@EntityScan(basePackages = "ru.practicum.statsdto")
 public class StatsServer {
     public static void main(String[] args) {
 
